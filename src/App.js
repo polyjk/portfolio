b1a2aac5103a2e210d2cb63376a65ccdc2fbb14d
John <p5khamm@gmail.comm>
2021-03-17 17:11:06 -0500

    Setup Routing Links

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter, Route, Link} from 'react-router-dom';
+import Projects from './Projects';
+import Articles from './Articles';
+import About from './About';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+
+        {/*Setup the router*/}
+        <Route exact path='/' component={Projects}/>
+        <Route path ='/Articles' component={Articles}/>
+        <Route path ='/About' component={About}/>
+        
+        <div className='navigation'>
+          <p className>Contact</p>
+          <div className='sub-navigation'>
+            <Link to='/' className='sub-nav-item'>Projects</Link>
+            <Link to='Articles' className='sub-nav-item'>Articles</Link>
+            <Link to='About' className='sub-nav-item'>About</Link>
+          </div>
+        
+        </div>
+      </div>
+    </BrowserRouter>
   );
 }
 
