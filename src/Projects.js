4bf1b5024517f41b28980394f96882c3cb0dbc8b
John <p5khamm@gmail.comm>
2021-03-30 15:21:33 -0500

    basic framer-motion implementation

--- src/Projects.js
+++ src/Projects.js
@@ -3,44 +3,52 @@ import Thumbnail from "./Thumbnail.js";
 import "./App.css";
 import logo from "./logo.svg";
 import { Col, Container, Row } from "react-bootstrap";
-import { motion } from "framer-motion";
+import { motion, useAnimation } from "framer-motion";
 import { Link } from "react-scroll";
 
 function Projects(props) {
+  const controls = useAnimation();
+  controls.start({
+    // scale: 1.025,
+    scale: [0.5, 1],
+    transition: { ease: "easeOut", duration: 0.5, staggerChildren: 0.1 },
+  });
   return (
     <Container>
       <h1 className="component-header">P</h1>
 
       {/* <Row className='projRow'> */}
-      <Container className="projects-all">
-        <Row>
-          <Thumbnail
-            link="/twitter"
-            image={logo}
-            title="Alpha"
-            text="Desc1"
-            smallTag="3/01/21"
-          />
-        </Row>
-        <Row>
-          <Thumbnail
-            link="/airbnb"
-            image={logo}
-            title="Beta"
-            text="Desc2"
-            smallTag="3/05/21"
-          />
-        </Row>
-        <Row>
-          <Thumbnail
-            link="/photoshop"
-            image={logo}
-            title="Chrome"
-            text="Desc3"
-            smallTag="3/015/21"
-          />
-        </Row>
-      </Container>
+      <motion.div animate={controls}>
+        <Container className="projects-all">
+          <Row>
+            <Thumbnail
+              link="/twitter"
+              image={logo}
+              title="Alpha"
+              text="Desc1"
+              smallTag="3/01/21"
+            />
+          </Row>
+          <Row>
+            <Thumbnail
+              link="/airbnb"
+              image={logo}
+              title="Beta"
+              text="Desc2"
+              smallTag="3/05/21"
+            />
+          </Row>
+          <Row>
+            <Thumbnail
+              link="/photoshop"
+              image={logo}
+              title="Chrome"
+              text="Desc3"
+              smallTag="3/015/21"
+            />
+          </Row>
+        </Container>
+      </motion.div>
     </Container>
   );
 }
